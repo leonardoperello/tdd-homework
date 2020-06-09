5bbc2ce132452ca784ed8b5e498d22b4a8de098c
leonardo.perello <leo_16.04.96@hotmail.com>
2020-06-09 12:09:14 -0300

    primer test (el de la clase)

---
+++ bowling/funcion1.py
@@ -0,0 +1,3 @@
+
+def play_bowling(current_score, rolls, pins):
+    return current_score + rolls * pins 
---
+++ bowling/test1.py
@@ -0,0 +1,5 @@
+from funcion1 import play_bowling
+
+def test_all_gutter():
+    result = play_bowling(current_score = 0,  rolls = 20, pins = 0)
+    assert result == 0
