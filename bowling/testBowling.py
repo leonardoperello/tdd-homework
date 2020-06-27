f50e77ae9161775b4243754917edfcee81336545
leonardo.perello <leo_16.04.96@hotmail.com>
2020-06-27 12:59:12 -0300

    RED testSpare

--- bowling/testBowling.py
+++ bowling/testBowling.py
@@ -5,3 +5,8 @@ from bowling import Game
 def testStrike():
     Game.roll(Game, 10)
     assert Game.strike(Game, 0) == 10 
+
+def testSpare():
+    Game.roll(Game, 5)
+    Game.roll(Game, 4)
+    assert Game.spare(Game, 1) == True
