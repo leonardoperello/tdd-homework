d59f3677cab7ba0f2d21cf099412beebaa749c70
leonardo.perello <leo_16.04.96@hotmail.com>
2020-06-27 12:16:47 -0300

    rama finalizada

--- bowling/funcion1.py
+++
@@ -1,3 +0,0 @@
-
-def play_bowling(current_score, rolls, pins):
-    return current_score + rolls * pins 
--- bowling/testStrike.py
+++ bowling/testStrike.py
@@ -2,7 +2,6 @@
 
 from bowling import Game
 
-    
 def testStrike():
     Game.roll(Game, 10)
     assert Game.strike(Game, 0) == 10 
