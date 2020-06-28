2ef4ceaf87a86df60dc3cb4098ba9f0b6b1f85fa
Carolina Villegas <caro.villegas63@gmail.com>
2020-06-28 17:35:07 -0300

    fix of juego perfecto

--- bowling/bowling.py
+++ bowling/bowling.py
@@ -2,12 +2,27 @@
 class Game:
     # rolls deberia tener entre 20 y 22 elementos ya que representa a los turnos para tirar
     # y cada posicion deberia tener el numero de pinos que se tiraron en ese turno
-    current_score = 0
+    
     totalPins = 10
     rolls = []
-
+        
     def roll(self, pins):
         self.rolls.append(pins)
+
+    def scoreFinal(self):
+        actualRoll = 0
+        current_score = 0
+        for x in range(10):
+            if(Game.strike(self, actualRoll)):
+                current_score += 10 + self.rolls[actualRoll+1] + self.rolls[actualRoll+2]
+                actualRoll += 1
+            elif (Game.spare(self, actualRoll)):
+                current_score += 10 + self.rolls[actualRoll+2]
+                actualRoll += 2
+            else:
+                current_score += self.rolls[actualRoll] + self.rolls[actualRoll+1]
+                actualRoll += 2
+        return current_score
 
     #metodo spare
     def spare(self, roll_):
@@ -21,30 +36,11 @@ class Game:
     def pinsEnPosicion(self, roll_):
         return self.rolls[roll_]
     
-    def muchos_rolls(self, pins, num):
-        for i in range(num):
-            Game.roll(self, pins)
-
     #determina si termino el juego (iniciado, por eso entre el self) o no
     def juegoTerminado(self):
         return((len(self.rolls)==20) | len(self.rolls)==22)
 
-    #creo que el puntaje perfecto es 300, CREO
-    def puntajePerfecto(self):
-        return(self.current_score==300)
+    def muchos_rolls(self, pins, num):
+        for i in range(num):
+            Game.roll(self, pins)
 
-    def scoreFinal(self):
-        #actualRoll = 0
-        current_score = 0
-        for actualRoll in range(9):
-            if(self.strike(actualRoll)):
-                current_score += 10 + self.rolls[actualRoll+1] + self.rolls[actualRoll+2]
-                actualRoll += 1
-            elif (self.rolls[actualRoll] + self.rolls[actualRoll+1] == self.totalPins):
-                current_score += 10 + self.rolls[actualRoll+2]
-                actualRoll += 2
-            else:
-                current_score += self.rolls[actualRoll] + self.rolls[actualRoll+1]
-                actualRoll += 2
-        return current_score
-    
--- bowling/testBowling.py
+++ bowling/testBowling.py
@@ -1,7 +1,7 @@
 
 
 from bowling import Game
-
+'''
 def muchos_rolls(pins, num):
     for i in range(num):
        Game.roll(Game, pins)
@@ -15,14 +15,19 @@ def testSpare():
    Game.roll(Game, 5)
    Game.roll(Game, 5)
    assert Game.spare(Game, 1) == True
-'''
+
 def test_gutter_game():
     Game.muchos_rolls(Game, 0, 20)
     assert Game.scoreFinal(Game) == 0
 
-def testPuntajePerfecto():
-    assert (Game.puntajePerfecto(Game)== True)
+def testScore():
+    Game.muchos_rolls(Game, 10, 12)
+    assert (Game.scoreFinal(Game)==300)
 '''
+def testPuntajePerfecto():
+    Game.muchos_rolls(Game, 10, 12)
+    assert (Game.scoreFinal(Game)==300)
+
 def testRandom():
     #for pins in [1, 4, 4, 5, 6, 4, 5, 5,
           #           7, 0, 1, 7, 3, 6, 4, 8, 2, 8, 6]:
