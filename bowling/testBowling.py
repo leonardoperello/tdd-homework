3523881c94bd59144867b28940c5793cc0b1d9f9
leonardo.perello <leo_16.04.96@hotmail.com>
2020-06-28 19:51:20 -0300

    RED testRandom

--- bowling/testBowling.py
+++ bowling/testBowling.py
@@ -14,22 +14,21 @@ from bowling import Game
 #def test_gutter_game():
 #    Game.muchos_rolls(Game, 0, 20)
 #   assert Game.scoreFinal(Game) == 0 
-
-#def testRandom():
-#    for pins in [10, 10, 10, 10, 10, 10, 10, 10,
-#                     10, 10, 10, 10]:
-#            Game.roll(Game,pins)
-    #Game.muchos_rolls(Game, 10, 12)
-#    assert Game.scoreFinal(Game) == 300
-
+'''
+def testRandom():
+    for pins in [1, 5, 7, 2, 9, 0, 8, 1,
+                     10, 3, 2, 5, 3, 4, 1, 1, 1]:
+            Game.roll(Game,pins)
+    assert Game.scoreFinal(Game) == 300
+'''
 def testScore():
     Game.muchos_rolls(Game, 10, 12)
     assert (Game.scoreFinal(Game)==300)
-'''
+
 def testUnos():
         Game.muchos_rolls(Game, 1, 20)
         assert Game.scoreFinal(Game) == 20
-'''
+
 def test_muchos_strikes():
     Game.roll(Game, 10)
     Game.roll(Game, 7)
