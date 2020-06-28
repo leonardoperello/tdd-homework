4f856f5b6e0343360b7ed0c8ca48485d6bfdf5a5
leonardo.perello <leo_16.04.96@hotmail.com>
2020-06-28 12:27:19 -0300

    iniciando test para muchos_rolls

--- bowling/testBowling.py
+++ bowling/testBowling.py
@@ -2,11 +2,19 @@
 
 from bowling import Game
 
-def testStrike():
-    Game.roll(Game, 10)
-    assert Game.strike(Game, 0) == 10 
+def muchos_rolls(pins, num):
+    for i in range(num):
+        Game.roll(Game, pins)
 
-def testSpare():
-    Game.roll(Game, 5)
-    Game.roll(Game, 10)
-    assert Game.spare(Game, 1) == True
+#def testStrike():
+ #   Game.roll(Game, 10)
+  #  assert Game.strike(Game, 0) == 10 
+
+#def testSpare():
+ #   Game.roll(Game, 5)
+ #   Game.roll(Game, 5)
+ #   assert Game.spare(Game, 1) == True
+
+def test_gutter_game():
+    muchos_rolls(0, 20)
+    assert Game.strike(Game, 20) == 0
