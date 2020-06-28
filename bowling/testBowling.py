729db2508d36bff91ad811827c0cad0d62ef3a3d
leonardo.perello <leo_16.04.96@hotmail.com>
2020-06-28 13:19:03 -0300

    borre algo que quedo de mas del ultimo merge

--- bowling/testBowling.py
+++ bowling/testBowling.py
@@ -12,10 +12,6 @@ def testSpare():
 def testPuntajePerfecto():
     assert (Game.puntajePerfecto(Game)== True)
 
-   Game.roll(Game, 5)
-   Game.roll(Game, 5)
-   assert Game.spare(Game, 1) == True
-
 def test_gutter_game():
     Game.muchos_rolls(Game, 0, 20)
     assert Game.pinsEnPosicion(Game, 19) == 1 
