aeebe7db44985968c7e1449285c879ac77f5502c
leonardo.perello <leo_16.04.96@hotmail.com>
2020-06-28 16:41:08 -0300

    RED test gutter game

--- bowling/bowling.py
+++ bowling/bowling.py
@@ -2,27 +2,27 @@
 class Game:
     # rolls deberia tener entre 20 y 22 elementos ya que representa a los turnos para tirar
     # y cada posicion deberia tener el numero de pinos que se tiraron en ese turno
-    current_score = 0
+    
     totalPins = 10
     rolls = []
-
+        
     def roll(self, pins):
         self.rolls.append(pins)
 
     def scoreFinal(self):
-        rollsPrincipales = 20
         actualRoll = 0
-        self.rolls[rollsPrincipales]
+        current_score = 0
         for x in range(10):
-            if(self.strike(actualRoll)):
+            if(Game.strike(self, actualRoll)):
                 current_score += 10 + self.rolls[actualRoll+1] + self.rolls[actualRoll+2]
                 actualRoll += 1
-            elif (self.spare(actualRoll)):
+            elif (Game.spare(self, actualRoll)):
                 current_score += 10 + self.rolls[actualRoll+2]
                 actualRoll += 2
             else:
                 current_score += self.rolls[actualRoll] + self.rolls[actualRoll+1]
                 actualRoll += 2
+        return current_score
 
     #metodo spare
     def spare(self, roll_):
@@ -41,11 +41,11 @@ class Game:
         return((len(self.rolls)==20) | len(self.rolls)==22)
 
     #el puntaje perfecto es 300
-    def puntajePerfecto(self):
-        return(self.current_score==300)
+    #def puntajePerfecto(self):
+    #    return Game.current_score
 
     def muchos_rolls(self, pins, num):
         for i in range(num):
-            self.roll(Game,pins)
+            Game.roll(self, pins)
 
     
--- bowling/testBowling.py
+++ bowling/testBowling.py
@@ -1,21 +1,27 @@
 from bowling import Game
 
-def testStrike():
-    Game.roll(Game, 10)
-    assert Game.strike(Game, 0)
 
-def testSpare():
-    Game.roll(Game, 5)
-    Game.roll(Game, 5)
-    assert Game.spare(Game, 1)
+#def testStrike():
+#    Game.roll(Game, 10)
+#    assert Game.strike(Game, 0)
 
-def testPuntajePerfecto():
-    Game.muchos_rolls(Game, 10, 12)
-    Game.scoreFinal
-    assert (Game.puntajePerfecto(Game) == True)
+#def testSpare():
+#   Game.roll(Game, 5)
+#   Game.roll(Game, 5)
+#   assert Game.spare(Game, 0)
+
+#def testPuntajePerfecto():
+ #   Game.muchos_rolls(Game, 10, 12)
+ #   assert (Game.scoreFinal(Game) == 300)
 
 def test_gutter_game():
     Game.muchos_rolls(Game, 0, 20)
-    Game.scoreFinal
-    assert Game.current_score == 0 
+    assert Game.scoreFinal(Game) == 0 
+
+#def testRandom():
+#    for pins in [10, 10, 10, 10, 10, 10, 10, 10,
+#                     10, 10, 10, 10]:
+#            Game.roll(Game,pins)
+    #Game.muchos_rolls(Game, 10, 12)
+#    assert Game.scoreFinal(Game) == 300
 
